ca9380d4e227930012a493ff90b9a986496acdc6
harry <harry@qq.com>
2019-05-21 20:13:17 +0800

    准备数据

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -1,5 +1,19 @@
 package com.atguigu.git;
 
 public class Git {
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+	}
 
 }
